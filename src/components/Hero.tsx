7f9c5df59615f70a8e7fef0e5131fa7b9779d0ae
gappeagy <appeagyeigeorgina@gmail.com>
2025-10-21 10:53:44 +0100

    projects button-style link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,8 +9,12 @@ const Hero: React.FC = () => {
            <h1 className="text-5xl font-bold text-gray-800 mb-4">
               Hi, I'm <span className="text-indigo-500">Georgina</span>
             </h1> 
-
-
+            <p className="text-lg text-gray-600 mb-8">
+              Developer
+            </p>
+            <a  href="#projects" className="px-6 py-3 bg-indigo-500 text-white rounded-full shadow hover:bg-indigo-600 transition">
+             Projects
+            </a>
         </section>
     )
 }
