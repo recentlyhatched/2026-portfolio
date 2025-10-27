5accfcb512b351d2df4facdd00394b91b135107f
gappeagy <appeagyeigeorgina@gmail.com>
2025-10-27 09:53:29 +0000

    add links component

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,14 +1,29 @@
 import React from "react"
-// import { FaGithub, FaGitlab, FaEnvelope } from "react-icons/fa"
+import { FaGithub, FaGitlab } from "react-icons/fa"
 
 const Links: React.FC = () => {
     return(
         <section
-          id="contact"
+          id="links"
           className="py-20 bg-gradient-to-t from-gray-50 to-white text-center text-gray-700"
           >
             <h2 className="text-3xl font-semibold mb-8 text-gray-800">Quick links</h2>
-            
+            <div className="flex justify-center space-x-6 text-3xl">
+                <a
+                  href="https://github.com/recentlyhatched"
+                  className="hover:text-indigo-500 transition"
+                  aria-label="GitHub"
+                >
+                    <FaGithub />
+                </a>
+                <a
+                  href="https://gitlab.com/recentlyhatched"
+                  className="hover:text-indigo-500 transition"
+                  aria-label="GitLab"
+                >
+                    <FaGitlab />
+                </a>
+            </div>
         </section>
     )
 }
